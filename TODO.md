04fa6799f26e473c1fc2de3881c4db389dff2559
Miguel Madero <git@miguelmadero.com>
2016-05-29 12:54:23 -0300

    Makes it easier to test the “hot” reload behavior

--- TODO.md
+++ TODO.md
@@ -1,7 +1,8 @@
 # TODOS
 
-- [ ] Remove old templates from the container and ask the resolver again for the new one instead or relying on the module path
+- [x] Make it easier to test "simulated" hot reload from the UI
 - [ ] Make this work with Ember stable (2.5 today). Setting the template doesn't refresh. Seems like we need to set it on component.block.template (not sure about layout)
+- [ ] Remove old templates from the container and ask the resolver again for the new one instead or relying on the module path
 - [ ] Test to make sure this works for layouts as well as templates. 
 - [ ] Add hot-reload for component classes
 - [ ] Add tests 
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,6 +1,23 @@
 import Ember from 'ember';
 
+const {inject} = Ember;
+
 export default Ember.Controller.extend({
+  hotReload: inject.service(),
+  ajax: Ember.inject.service(),
   now: new Date(),
-  externalCounter: 1
+  externalCounter: 1,
+  actions: {
+    updateTemplate() {
+      // This simulates a change to the template and a rebuild.
+      // Once we have everything hooked together, when ember-cli rebuilds
+      // we will trigger an event, handled by the hotReload, which will attemp
+      // to download the module and if it finds it, eval it then trigger an event
+      // we do this two steps manually to simulate that part.
+      this.get('ajax').request('updated-template.js', {method: 'GET', dataType: 'text'}).then(template=>{
+        eval(template);
+        this.get('hotReload').trigger('newChanges', 'dummy/templates/components/my-component');
+      })
+    }
+  }
 });
